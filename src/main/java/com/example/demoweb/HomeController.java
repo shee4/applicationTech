6da5edfe86cf8aba80ac35feea32d05e5a7846c4
shee4 <lilite71@gmail.com>
2019-09-06 15:38:22 +0500

    changing port to 8000

--- src/main/java/com/example/demoweb/HomeController.java
+++ src/main/java/com/example/demoweb/HomeController.java
@@ -3,21 +3,10 @@ package com.example.demoweb;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-
 @RestController
 public class HomeController {
     @RequestMapping("/")
     public String home(String text){
-        try {
-            URL url = new URL("localhost:8080");
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestMethod("POST");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return text;
     }
 }
