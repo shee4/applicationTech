eb68c8599f3e462c4c607a7500846e7ebb8735ea
shee4 <lilite71@gmail.com>
2019-09-06 15:27:54 +0500

    1st commit

---
+++ src/main/java/com/example/demoweb/Car.java
@@ -0,0 +1,22 @@
+package com.example.demoweb;
+
+public class Car {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIssueYear() {
+        return issueYear;
+    }
+
+    public void setIssueYear(String issueYear) {
+        this.issueYear = issueYear;
+    }
+
+    String name;
+    String issueYear;
+}
